4719b3b53caa0b29b7fabd8c7fe0f6492e32c523
zz <517669936@qq.com>
2023-08-10 19:53:16 +0800

    fix: fix init for struct

--- Sources/ios-custom-auth-auth-sdk/SmartAccountTypes.swift
+++ Sources/ios-custom-auth-auth-sdk/SmartAccountTypes.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Shared
+import web3
 
 public enum ChainID: UInt64 {
     case ETHEREUM_MAINNET = 1
@@ -29,17 +30,26 @@ public protocol SignerExt {
     func signMessage(message: String) -> String
 }
 
-extension SignerExt where Self: Signer{
+public extension Signer where Self:web3.EthereumAccountProtocol{
+        
+}
+
+public extension SignerExt where Self: Signer{
     func signMessage(message: String) -> String{
         return self.signMessage(message: message.data(using: String.Encoding.utf8)!);
     }
-
 }
 
 public struct ChainOptions {
     public var chainId: ChainID = ChainID.ETHEREUM_MAINNET
     public var rpcUrl: String
     public var relayerUrl: String?
+    
+    public init(chainId: ChainID, rpcUrl: String, relayerUrl: String? = nil) {
+        self.chainId = chainId
+        self.rpcUrl = rpcUrl
+        self.relayerUrl = relayerUrl
+    }
 }
 
 public struct SmartAccountOptions {
@@ -48,15 +58,27 @@ public struct SmartAccountOptions {
     public var appId: String
     public var unipassServerUrl: String?
     public var chainOptions: Array<ChainOptions>
+    
+    public init(masterKeySigner: Signer? = nil, masterKeyRoleWeight: RoleWeight? = nil, appId: String, unipassServerUrl: String? = nil, chainOptions: Array<ChainOptions>) {
+        self.masterKeySigner = masterKeySigner
+        self.masterKeyRoleWeight = masterKeyRoleWeight
+        self.appId = appId
+        self.unipassServerUrl = unipassServerUrl
+        self.chainOptions = chainOptions
+    }
 }
 
 public struct SmartAccountInitOptions{
     public var chainId:ChainID
+    public init(chainId: ChainID) {
+        self.chainId = chainId
+    }
 }
 
 internal class WrapSigner: Shared.Signer {
     private var signer: Signer
-    init(signer: Signer) {
+    
+    public init(signer: Signer) {
         self.signer = signer
     }
 
